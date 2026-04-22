3f10574dc951605b5e4bb0d8399a1a80535e0580
rahulwaghel007 <rahulwaghel007@gmail.com>
2026-04-21 23:43:30 -0400

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Test {
+
+    // Method to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+       
+        System.out.println("Hello, Java is working!");
+
+   d
+        int result = add(5, 3);
+        System.out.println("Sum of 5 and 3 is: " + result);
+
+   
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Welcome, " + name + "!");
+
+        scanner.close();
+    }
+}
